4072b17f4b04d8b192924396c2db9f593e26b2b0
AnjaliRSahu <anjalisahu1996@gmail.com>
2022-09-16 14:26:48 +0530

    [anjali]InsertionSort&Anagram

---
+++ AlgorithmProblem/Anagram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblem
+{
+    public class Anagram
+    {
+        public static void AnagramDetection()
+        {
+            string str1 = "heart";
+            string str2 = "earth";
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1=new string(ch1);
+            string val2=new string(ch2);
+
+            Console.WriteLine(val1 + "  " + val2);
+
+            if (val1 == val2)
+                Console.WriteLine("Anagram");
+            else
+                Console.WriteLine("Not Anagram");
+
+        }
+    }
+}
---
+++ AlgorithmProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertionSort.SortArray();
+            Anagram.AnagramDetection();
+
+            Console.ReadLine();
+        }
+    }
+}
